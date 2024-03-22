180656c45bc78e0163f9d2db8f7c26f6f32352b1
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-03-22 11:04:02 +0530

    example .env and eaxample pbnscred.php

---
+++ example-pbnscred.php
@@ -0,0 +1,12 @@
+<?php
+	$servername = "fgydfgdgf";
+	$username = "fgdfg";
+	$password = "fgdfgfdg";
+	$dbname = "fdgfdgdfg";
+	// put domain name with out http://
+	$domainname="fgfdgfg.fdgfgg.fdgfdgg";
+	$mainfoldername = "fgdffdg";
+	$mainfoldernames3 = "fdgfdgfg";
+	$s3key="fdgfdgfdgdfg";
+	$s3secretkey="fdgdfgfdgfg";
+?>
